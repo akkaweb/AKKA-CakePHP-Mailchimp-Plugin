f2615ba1277c5b5c57256e5e0a4a1f0488f70b2c
AKKA Web Development <admin@akkaweb.com>
2013-12-03 23:51:33 -0500

    Update MailchimpAppController.php

--- Controller/MailchimpAppController.php
+++ Controller/MailchimpAppController.php
@@ -1,4 +1,12 @@
 <?php
+/*
+* Mailchimp AppController
+*
+* @copyright AKKA Web Development (http://www.akkaweb.com)
+* @author Andre Santiago <admin@akkaweb.com>
+* @license MIT License http://www.opensource.org/licenses/mit-license.php
+* @date Dec 3, 2013
+*/
 
 App::uses('AppController', 'Controller');
 
