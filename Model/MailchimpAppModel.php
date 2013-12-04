b5fefaa4a015be35265c9f8e439c353ad5f4f946
AKKA Web Development <admin@akkaweb.com>
2013-12-03 23:52:03 -0500

    Update MailchimpAppModel.php

--- Model/MailchimpAppModel.php
+++ Model/MailchimpAppModel.php
@@ -1,5 +1,12 @@
 <?php
-
+/*
+* Mailchimp AppModel
+*
+* @copyright AKKA Web Development (http://www.akkaweb.com)
+* @author Andre Santiago <admin@akkaweb.com>
+* @license MIT License http://www.opensource.org/licenses/mit-license.php
+* @date Dec 3, 2013
+*/
 App::uses('AppModel', 'Model');
 
 class MailchimpAppModel extends AppModel {
